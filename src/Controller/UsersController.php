749e7106809589dda592d4c78fef6e569344c60f
thomas rieder <thom.rieder@gmail.com>
2020-04-18 17:43:11 +0200

    ajout de la liste nouveaux utilisateurs avant le formulaire d'ajout

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -10,12 +10,50 @@ use Symfony\Component\Routing\Annotation\Route;
 class UsersController extends AbstractController {
 
     /**
-     * @Route("/add", name="add_user")
+     * @Route("/add", name="new_user_list")
      */
     public function addUser() {
+
+
+        //todo: chek if add user form was send
+
+        //list of user with new status
+        $newUsers = array(
+            array(
+                "id" => "1",
+                "tag_rfid" => "blblblb"
+            ),
+            array(
+                "id" => "2",
+                "tag_rfid" => "qwertzuiop"
+            ),
+            array(
+                "id" => "3",
+                "tag_rfid" => "asdfghjkl"
+            ),
+            array(
+                "id" => "4",
+                "tag_rfid" => "yxcvbn"
+            ),
+            array(
+                "id" => "5",
+                "tag_rfid" => "poiuztrew"
+            )
+        );
+
+        $data =  array('current_page' => 'new_user_list', "users" => $newUsers);
+
+        return $this->render('newUserList.html.twig', $data);
+    }
+
+    /**
+     * @Route("/addUser/{id}/{tag_rfid}", name="add_user")
+     */
+    public function newUser($id, $tag_rfid) {
         $data =  array(
             'current_page' => 'add_user',
-            "id" => "123"
+            'id' => $id,
+            'tag_rfid' => $tag_rfid
         );
         return $this->render('addUser.html.twig', $data);
     }
@@ -43,6 +81,9 @@ class UsersController extends AbstractController {
      */
     public function listUsers() {
 
+        //todo: check if edit user form was send !
+
+        //list of all users
         $users = array(
             array(
                 "id" => "1",
