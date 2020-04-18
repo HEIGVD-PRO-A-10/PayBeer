bd8b1dbeaa78ecabe54e5d85cb4b6f02501ed9af
Gil Balsiger <gil.balsiger@gmail.com>
2020-04-18 14:56:11 +0200

    Refactor controllers

--- src/Controller/TransactionsController.php
+++ src/Controller/TransactionsController.php
@@ -3,18 +3,24 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+class TransactionsController extends AbstractController {
 
-class HistoryController extends AbstractController {
+    /**
+     * @Route("/", name="home")
+     */
+    public function home() {
+        $data =  array('current_page' => 'home', "theme" => "red");
+        return $this->render('home.html.twig', $data);
+    }
 
     /**
      * @Route("/history", name="history")
      */
     public function history() {
-
-
         $data =  array('current_page' => 'history', "theme" => "red");
         return $this->render('history.html.twig', $data);
     }
--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -3,10 +3,40 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UsersListController extends AbstractController {
+class UsersController extends AbstractController {
+
+    /**
+     * @Route("/add", name="add_user")
+     */
+    public function addUser() {
+        $data =  array(
+            'current_page' => 'add_user',
+            "id" => "123"
+        );
+        return $this->render('addUser.html.twig', $data);
+    }
+
+    /**
+     * @Route("/edit/{userID}", name="edit_user")
+     */
+    public function editUser($userID) {
+
+        //TODO: check id exist and get user info with id
+
+        $data =  array(
+            'current_page' => 'edit_user',
+            'id' => $userID,
+            'name' => 'Rieder',
+            'firstname' => 'Thomas',
+            'amount' => 1000,
+            'active' => 1
+        );
+        return $this->render('editUser.html.twig', $data);
+    }
 
     /**
      * @Route("/list", name="list_user")
