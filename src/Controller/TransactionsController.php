83d9b57133e1797b87cc34870f44b5a59f81a811
Gil Balsiger <gil.balsiger@gmail.com>
2020-04-21 23:54:22 +0200

    Transaction edition

--- src/Controller/TransactionsController.php
+++ src/Controller/TransactionsController.php
@@ -16,7 +16,7 @@ class TransactionsController extends AbstractController {
      * @Route("/history", name="history")
      */
     public function history(Request $request, TransactionRepository $transactionRepository) {
-        if($query = $request->query->get('q')) {
+        if ($query = $request->query->get('q')) {
             //$transactions = $transactionRepository->searchByLastnameOrFirstname($query);
             return $this->render('transactions/history.html.twig', ['transactions' => [], 'query' => $query]);
         } else {
@@ -33,7 +33,7 @@ class TransactionsController extends AbstractController {
      */
     public function info($id, TransactionRepository $repo) {
         $transaction = $repo->find($id);
-        if($transaction) {
+        if ($transaction) {
             return $this->render('transactions/info.html.twig', ['transaction' => $transaction]);
         } else {
             return $this->render('404.html.twig');
@@ -50,11 +50,12 @@ class TransactionsController extends AbstractController {
     public function cancel($id, Request $request, TransactionRepository $repo) {
         $manager = $this->getDoctrine()->getManager();
         $transaction = $repo->find($id);
-        if($transaction) {
+        if ($transaction) {
             $transaction->setStatus('CANCELED');
             $manager->flush();
+            $this->addFlash('success', "<i class='fa fa-minus-circle text-danger'></i> La transaction {$id} est annulée.");
             $referer = $request->headers->get('referer');
-            if($referer) {
+            if ($referer) {
                 return $this->redirect($referer);
             } else {
                 return $this->redirectToRoute('transaction_info', ['id' => $id]);
@@ -74,11 +75,12 @@ class TransactionsController extends AbstractController {
     public function maintain($id, Request $request, TransactionRepository $repo) {
         $manager = $this->getDoctrine()->getManager();
         $transaction = $repo->find($id);
-        if($transaction) {
+        if ($transaction) {
             $transaction->setStatus(null);
             $manager->flush();
+            $this->addFlash('success', "<i class='fa fa-check'></i> La transaction {$id} est maintenue.");
             $referer = $request->headers->get('referer');
-            if($referer) {
+            if ($referer) {
                 return $this->redirect($referer);
             } else {
                 return $this->redirectToRoute('transaction_info', ['id' => $id]);
@@ -86,6 +88,47 @@ class TransactionsController extends AbstractController {
         } else {
             return $this->render('404.html.twig');
         }
+    }
+
+    /**
+     * @Route("/transaction/{id}/edit", name="transaction_edit", methods={"GET"})
+     * @param $id
+     * @param TransactionRepository $repo
+     * @return Response
+     */
+    public function edit($id, TransactionRepository $repo) {
+        $transaction = $repo->find($id);
+        if ($transaction) {
+            return $this->render('transactions/edit.html.twig', ['transaction' => $transaction]);
+        } else {
+            return $this->render('404.html.twig');
+        }
+    }
+
+    /**
+     * @Route("/transaction/{id}/edit", name="transaction_edit_post", methods={"POST"})
+     * @param $id
+     * @param Request $request
+     * @param TransactionRepository $repo
+     * @return Response
+     */
+    public function editPost($id, Request $request, TransactionRepository $repo) {
+        $manager = $this->getDoctrine()->getManager();
+        $transaction = $repo->find($id);
+        if ($transaction) {
+            if ($amount = $request->request->get('amount')) {
+                if(is_numeric($amount)) {
+                    $transaction->setAmount($amount);
+                    $manager->flush();
+                    $this->addFlash('success', '<i class=\'fa fa-check\'></i> La transaction a bien été mise à jour.');
+                    return $this->redirectToRoute('transaction_info', ['id' => $id]);
+                } else {
+                    $this->addFlash('danger', 'Le montant doit être un nombre.');
+                    return $this->redirectToRoute('transaction_edit', ['id' => $id]);
+                }
+            }
+        }
+        return $this->render('404.html.twig');
     }
 
 }
